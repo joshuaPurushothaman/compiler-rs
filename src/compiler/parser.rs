ae1421bc498dc3d495aa355d1f1bf97812a35ff0
Joshua Purushothaman <joshuathooyavan1@gmail.com>
2026-03-20 01:40:22 -0500

    Added parens!

--- src/compiler/parser.rs
+++ src/compiler/parser.rs
@@ -8,37 +8,39 @@ use chumsky::{
 };
 
 pub fn parser<'src>() -> impl Parser<'src, &'src str, Expr<'src>> {
-    let int = text::int(10)
-        .map(|s: &str| Expr::Num(s.parse().unwrap()))
-        .padded();
+    recursive(|expr| {
+        let int = text::int(10)
+            .map(|s: &str| Expr::Num(s.parse().unwrap()))
+            .padded();
 
-    let atom = int;
+        let atom = int.or(expr.delimited_by(just('('), just(')'))).padded();
 
-    let op = |c| just(c).padded();
+        let op = |c| just(c).padded();
 
-    let unary = op('-')
-        .repeated()
-        .foldr(atom, |_op, rhs| Expr::Neg(Box::new(rhs)));
+        let unary = op('-')
+            .repeated()
+            .foldr(atom, |_op, rhs| Expr::Neg(Box::new(rhs)));
 
-    let product = unary.foldl(
-        choice((
-            op('*').to(Expr::Mul as fn(_, _) -> _),
-            op('/').to(Expr::Div as fn(_, _) -> _),
-        ))
-        .then(unary)
-        .repeated(),
-        |lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)),
-    );
+        let product = unary.clone().foldl(
+            choice((
+                op('*').to(Expr::Mul as fn(_, _) -> _),
+                op('/').to(Expr::Div as fn(_, _) -> _),
+            ))
+            .then(unary)
+            .repeated(),
+            |lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)),
+        );
 
-    let sum = product.foldl(
-        choice((
-            op('+').to(Expr::Add as fn(_, _) -> _),
-            op('-').to(Expr::Sub as fn(_, _) -> _),
-        ))
-        .then(product)
-        .repeated(),
-        |lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)),
-    );
+        let sum = product.clone().foldl(
+            choice((
+                op('+').to(Expr::Add as fn(_, _) -> _),
+                op('-').to(Expr::Sub as fn(_, _) -> _),
+            ))
+            .then(product)
+            .repeated(),
+            |lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)),
+        );
 
-    sum
+        sum
+    })
 }
